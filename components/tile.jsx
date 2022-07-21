9a8005e5d0fa791b3fe9f97e600e58efe2d990e7
Subhrakanti Dasgupta <subhrakanti.dasgupta@gmail.com>
2022-07-21 22:34:54 +0530

    Update tile.jsx

--- components/tile.jsx
+++ components/tile.jsx
@@ -8,7 +8,7 @@ export default function Tile({ index, post }) {
                 <p className = "text-white text-justify">{ gist }</p>
             </div>
             {
-                image && <img src = { 'https:' + image.fields.file.url } className = { index % 2 === 0 ? "h-66 w-72 rounded-md rotate-6" : "h-66 w-72 rounded-md -rotate-6" } />
+                image && <img src = { 'https:' + image.fields.file.url } className = { index % 2 === 0 ? "h-48 w-48 rounded-md rotate-6" : "h-48 w-48 rounded-md -rotate-6" } />
             }
         </section>
     )
