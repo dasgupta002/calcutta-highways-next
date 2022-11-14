e774c2442280f49164d672d1c75987435d915c1a
Subhrakanti Dasgupta <subhrakanti.dasgupta@gmail.com>
2022-11-15 01:40:11 +0530

    Update post.jsx

--- components/post.jsx
+++ components/post.jsx
@@ -6,8 +6,10 @@ export default function Post({ post }) {
     return (
         <article className = "flex flex-col my-12">
             <h1 className = "text-4xl font-bold mb-16">{ title }</h1>
-            { image && <img src = { 'https:' + image.fields.file.url } className = "h-[15rem] md:h-[27rem] w-full md:w-1/2 md:self-center rounded-md rotate-6 mb-16" /> }
-            <div className = { type === 'Poem' ? 'text-center text-lg' : 'text-justify text-lg' }>{ documentToReactComponents(content) }</div>
+            <div className = "flex flex-row">
+                { image && <img src = { 'https:' + image.fields.file.url } className = "h-[15rem] md:h-[27rem] w-1/2 rounded-md rotate-6 mr-12 mb-12" /> }
+                <div className = { type === 'Poem' ? 'text-center text-lg' : 'text-justify text-lg' }>{ documentToReactComponents(content) }</div>
+            </div>
         </article>
     )
 }
